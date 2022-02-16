42cdbc2d06d15ac73f1b0c5a2ce82bee77092aa9
Rakesh Pandian <rakesh.pandian07@gmail.com>
2022-02-17 03:00:17 +0530

    CORS ISSUE

--- UserAPI/Program.cs
+++ UserAPI/Program.cs
@@ -10,6 +10,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllHeaders",
+          builder =>
+          {
+              builder.AllowAnyOrigin()
+                     .AllowAnyHeader()
+                     .AllowAnyMethod();
+          });
+});
 
 var app = builder.Build();
 
